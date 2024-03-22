62f5bcbabe08750fa6eef7aff79e1c498cf5de40
Maverick Grayer <maverick72032@gmail.com>
2024-03-22 17:42:23 -0400

    cleaned code

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -7,37 +7,6 @@ namespace InterfaceExercise
     {
         static void Main(string[] args)
         {
-            //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
-
-            //Create 2 Interfaces called IVehicle & ICompany
-
-            //Create 3 classes called Car , Truck , & SUV
-
-            //In your IVehicle:
-
-            /* Create 4 members that Car, Truck, & SUV all have in common.
-             * Example: public int NumberOfWheels { get; set; }
-             */ // COMPLETED
-
-
-            //In ICompany: 
-
-            /* Create 2 members that are specific to each every company
-             * regardless of vehicle type.
-             * Example: public string Logo { get; set; }
-             */ // COMPLETED
-
-            //In each of your Car, Truck, and SUV classes
-
-            /* Create 2 members that are specific to each class
-             * Example for Car: public bool HasTrunk { get; set; }
-             * Example for SUV: public int NumberOfSeats { get; set; }
-             *
-             * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
-             */
-
-            //Now, create objects of your 3 classes and give their members values.
-            //Creatively display and organize their values
             Car car1 = new Car()
             {
                 Model = "Chrysler",
